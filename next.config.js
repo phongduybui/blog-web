1518fa1a9a8bd80d16626a64ef105478ce42df9a
phongduybui <buiduyphong921@gmail.com>
2023-02-20 00:24:46 +0700

    fix: proxy

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ const nextConfig = {
     return [
       {
         source: '/uploads/:path*',
-        destination: process.env.NEXT_PUBLIC_SERVER_URL + '/uploads/:path*', // Proxy to Backend
+        destination: 'https://admin.hoangan.site/uploads/:path*', // Proxy to Backend
       },
     ];
   },
